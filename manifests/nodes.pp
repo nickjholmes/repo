ad2fcf6986eb0a288478463e72b2a5d02821c42c
nickjholmes <nick.holmes67@gmail.com>
2015-06-27 14:46:40 +0000

    updates

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,8 @@
 node 'njh67aws1' {
 	include puppet
 }
+node 'njh67aws'{
+	#include memcached
+	#include test
+	include admin::ntp
+}
---
+++ modules/test/manifests/init.pp
@@ -0,0 +1,8 @@
+class test {
+	define tmpfile() {
+		file { "/tmp/${name}":
+			content	=>	"Hello, world\n",
+		}
+	}
+	tmpfile { ['a', 'b', 'c']: }
+}

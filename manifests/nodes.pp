f854ea5d9a53f884e3d78e5ce5ba6fe9bf70bd55
nickjholmes <nick.holmes67@gmail.com>
2015-06-30 10:28:54 +0000

    updates

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,12 +2,17 @@ node 'njh67aws1' {
 	include puppet
 }
 node 'njh67aws'{
-	include admin::rsyncdconf
-
-	file { '/etc/rsyncd.d/myapp.conf':
-		ensure	=> present,
-		source 	=> 'puppet:///modules/admin/myapp.rsync',
-		require	=> File['/etc/rsyncd.d'],
-		notify	=> Exec['update-rsyncd.conf'],
+#	include admin::rsyncdconf
+#
+#	file { '/etc/rsyncd.d/myapp.conf':
+#		ensure	=> present,
+#		source 	=> 'puppet:///modules/admin/myapp.rsync',
+#		require	=> File['/etc/rsyncd.d'],
+#		notify	=> Exec['update-rsyncd.conf'],
+#	}
+	$mysql_password = 'secret'
+	file { '/usr/local/bin/backup-mysql':
+		content	=>	template('admin/backup-mysql.sh.erb'),
+		mode	=>	'0755',
 	}
 }

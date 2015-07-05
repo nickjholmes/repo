c552aecc320330a0e196a95fa76ffb58324905f2
nickjholmes <nick.holmes67@gmail.com>
2015-07-05 10:14:28 +0000

    Updates

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,17 +2,10 @@ node 'njh67aws1' {
 	include puppet
 }
 node 'njh67aws'{
-#	include admin::rsyncdconf
-#
-#	file { '/etc/rsyncd.d/myapp.conf':
-#		ensure	=> present,
-#		source 	=> 'puppet:///modules/admin/myapp.rsync',
-#		require	=> File['/etc/rsyncd.d'],
-#		notify	=> Exec['update-rsyncd.conf'],
-#	}
-	$mysql_password = 'secret'
-	file { '/usr/local/bin/backup-mysql':
-		content	=>	template('admin/backup-mysql.sh.erb'),
-		mode	=>	'0755',
+	include admin::percona_repo
+
+	package { 'percona-server-server-5.5':
+		ensure	=>	installed,
+		require	=>	Class['admin::percona_repo'],
 	}
 }

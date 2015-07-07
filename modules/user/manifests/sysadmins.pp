57148b77e8ba3014fd4f5e6a7caa4b6a93db0ee7
nickjholmes <nick.holmes67@gmail.com>
2015-07-07 13:28:21 +0000

    updates 7th July2015v2

--- modules/user/manifests/sysadmins.pp
+++ modules/user/manifests/sysadmins.pp
@@ -3,5 +3,8 @@ class user::sysadmins {
 #	realize(User['sarah'])
 	search User::Virtual
 
-	realize(Ssh_user['aliza'])
+#	realize(Ssh_user['aliza'])
+
+	realize(Ssh_user['jamil'])
+	realize(Ssh_user['scarlet'])
 }

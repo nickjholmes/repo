b03e8eb4ccc81a342de65ad52218edbc810c47a2
nickjholmes <nick.holmes67@gmail.com>
2015-07-19 09:40:55 +0000

    19July2015

--- modules/sinatra/manifests/init.pp
+++ modules/sinatra/manifests/init.pp
@@ -41,50 +41,60 @@ vcsrepo { '/opt/code/simple-sinatra-app':
 	source	=> 'git://github.com/tnh/simple-sinatra-app.git',
 	revision=> 'master',
 	}
+	
+# perform bundle install
+exec { 'bundle install':
+	command=> '/usr/bin/bundle install',
+	cwd=> '/opt/code/nginx-unicorn-sinatra',
+	require	=> [ Vcsrepo['/opt/code/simple-sinatra-app'] ],
+	}
+
+
+# create directories
+file { [ '/opt/code/nginx-unicorn-sinatra/var/', '/opt/code/nginx-unicorn-sinatra/var/run', '/opt/code/nginx-unicorn-sinatra/var/log', '/var/www' ]:
+    	ensure => directory,
+		mode =>	775,
+		require	=> [ Exec['bundle install'] ],
+	}
 
 # preferred symlink syntax
 file { '/var/www/nginx-unicorn-sinatra':
 	ensure => link,
 	target => '/opt/code/nginx-unicorn-sinatra',
-	require	=> [ Vcsrepo['/opt/code/simple-sinatra-app'] ],
-	}
+	require	=> [ File['/opt/code/nginx-unicorn-sinatra/var/', '/opt/code/nginx-unicorn-sinatra/var/run', '/opt/code/nginx-unicorn-sinatra/var/log', '/var/www'] ],
+	}	
+
 	
-exec { 'bundle install':
-	command=> '/usr/bin/bundle install',
-	cwd=> '/opt/code/nginx-unicorn-sinatra',
+# archive /etc/nginx/nginx.conf
+exec { 'copy /etc/nginx/nginx.conf':
+	command=> '/bin/cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig',
 	require	=> [ File['/var/www/nginx-unicorn-sinatra'] ],
 	}
 
-	
-
-# create directories
-file { [ '/opt/code/nginx-unicorn-sinatra/var/', '/opt/code/nginx-unicorn-sinatra/var/run', '/opt/code/nginx-unicorn-sinatra/var/log' ]:
-    	ensure => directory,
-		require	=> [ Exec['bundle install'] ],
-	}
-
-
-exec { 'copy /etc/nginx/nginx.conf':
-	command=> '/bin/cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig',
-	require	=> [ File['/opt/code/nginx-unicorn-sinatra/var/', '/opt/code/nginx-unicorn-sinatra/var/run', '/opt/code/nginx-unicorn-sinatra/var/log'] ],
-	}
-
+# copy in new nginx.conf
 file { '/etc/nginx/nginx.conf':
 	ensure => present,
 	source => '/opt/code/nginx-unicorn-sinatra/config/nginx.conf',
 	require	=> [ Exec['copy /etc/nginx/nginx.conf'] ],
 	}
 
+# archive /opt/code/nginx-unicorn-sinatra/config.ru
+exec { 'copy /opt/code/nginx-unicorn-sinatra/config.ru':
+	command=> '/opt/code/nginx-unicorn-sinatra/config.ru /opt/code/nginx-unicorn-sinatra/config.ru.orig',
+	require	=> [ File['/etc/nginx/nginx.conf'] ],
+	}
+	
+# copy in new /opt/code/nginx-unicorn-sinatra/config.ru
 file { '/opt/code/nginx-unicorn-sinatra/config.ru':
         ensure => present,
         source => '/opt/code/simple-sinatra-app/config.ru',
-		require	=> [ File['/etc/nginx/nginx.conf'] ],
+		require	=> [ Exec['copy /opt/code/nginx-unicorn-sinatra/config.ru'] ],
         }
 
 file { '/opt/code/nginx-unicorn-sinatra/helloworld.rb':
         ensure => present,
         source => '/opt/code/simple-sinatra-app/helloworld.rb',
-	require	=> [ File['/opt/code/nginx-unicorn-sinatra/config.ru'] ],
+		require	=> [ File['/opt/code/nginx-unicorn-sinatra/config.ru'] ],
         }
 
 }
